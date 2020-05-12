6608d4dfa29ad670d9139b4c5dc4dde4cffd7ed3
Ajay Ravindran <ajaycravindran@gmail.com>
2020-05-12 20:05:57 +0530

    Create find_faces.py

---
+++ videoAnalytics/src/data layer/find_faces.py
@@ -0,0 +1,4 @@
+# Accepts a particular image and locates the different faces in the image, 
+# forming a table of images and the coordinates of the centre of the image as output.
+
+def locate_faces(inout_image) :
