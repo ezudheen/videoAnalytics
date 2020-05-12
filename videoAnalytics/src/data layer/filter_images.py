45eff6a16ab08dfaba14a9233ffe405adbee78fd
Ajay Ravindran <ajaycravindran@gmail.com>
2020-05-12 18:20:44 +0530

    Create filter_images.py

---
+++ videoAnalytics/src/data layer/filter_images.py
@@ -0,0 +1,4 @@
+# Accepts the video and creates a set of images by removing frames that have reduntant information,
+# using an efficient differentiator and a threshold value
+
+def filterImages(input_video) :
