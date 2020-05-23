1cab71f7d7e29e0eff2b091a7cb3ef0f204c42de
Ajay Ravindran <ajaycravindran@gmail.com>
2020-05-23 12:59:11 +0530

    Delete filter_images.py

--- videoAnalytics/src/data layer/filter_images.py
+++
@@ -1,4 +0,0 @@
-# Accepts the video and creates a set of images by removing frames that have reduntant information,
-# using an efficient differentiator and a threshold value
-
-def filterImages(input_video) :
